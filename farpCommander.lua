da3b2b1e7ce2198c4cfd2964bcbf0a1692397832
Dzsek <dzsekeb@gmail.com>
2021-11-06 19:12:38 +0200

    added demo mission and fixed various bugs after initial tests

--- farpCommander.lua
+++ farpCommander.lua
@@ -5,7 +5,7 @@ farpCommander = {}
 --	{ tozone = "c2", supplyUnit = "redcap1", attackGroup = "redattack1" }
 --}
 
---farpCommander.addFarp("c1", 0, 0, {"redcap1","bluecap1"}, { "blue1", "blue2", "blue3"}, { "red1", "red2", "red3"}, {"blueCargo1"}, dispatcher)
+--farpCommander.addFarp("c1", 0, 0, {"redcap1","bluecap1"}, { "blue1", "blue2", "blue3"}, { "red1", "red2", "red3"}, {"blueCargo1"} , reddp, bluedp)
 --farpCommander.init()
 
 do
@@ -45,7 +45,7 @@ do
 				
 				for i2,v2 in pairs(upgrades) do
 					if not v.builtupgrades[i2] and i2<=v.level then
-						local gr = mist.cloneInZone(v2, v.zone)
+						local gr = mist.cloneInZone(v2, v.zone, nil, nil, {initTasks=true, validTerrain={'LAND'}})
 						v.builtupgrades[i2] = gr.name
 					end
 				end
@@ -62,40 +62,60 @@ do
 			trigger.action.circleToAll(-1,i,zone.point, zone.radius,color,color,1)
 			trigger.action.textToAll(-1,2000+i,zone.point, {0,0,0,0.5}, {0,0,0,0}, 15, true, v.zone)
 			
+			-- if v.side == 0 then
+				-- trigger.action.smoke(farpCommander.getPointOnSurface(zone.point), trigger.smokeColor.Green)
+			-- elseif v.side == 1 then
+				-- trigger.action.smoke(farpCommander.getPointOnSurface(zone.point), trigger.smokeColor.Red)
+			-- elseif v.side == 2 then
+				-- trigger.action.smoke(farpCommander.getPointOnSurface(zone.point), trigger.smokeColor.Blue)
+			-- end
+			
 			if v.redDispatcher then
 				for i2,v2 in ipairs(v.redDispatcher) do
-					local u = Unit.getByName(v2.supplyUnit)
-					if u then
-						u:destroy()
+					if v2.supplyUnit then
+						local u = Unit.getByName(v2.supplyUnit)
+						if u then
+							u:destroy()
+						end
 					end
 					
-					local gr = Group.getByName(v2.attackGroup)
-					if gr then
-						gr:destroy()
+					if v2.attackGroup then
+						local gr = Group.getByName(v2.attackGroup)
+						if gr then
+							gr:destroy()
+						end
 					end
 					
-					local gr = Group.getByName(v2.cap)
-					if gr then
-						gr:destroy()
+					if v2.cap then
+						local gr = Group.getByName(v2.cap)
+						if gr then
+							gr:destroy()
+						end
 					end
 				end
 			end
 			
 			if v.blueDispatcher then
 				for i2,v2 in ipairs(v.blueDispatcher) do
-					local u = Unit.getByName(v2.supplyUnit)
-					if u then
-						u:destroy()
+					if v2.supplyUnit then
+						local u = Unit.getByName(v2.supplyUnit)
+						if u then
+							u:destroy()
+						end
 					end
 					
-					local gr = Group.getByName(v2.attackGroup)
-					if gr then
-						gr:destroy()
+					if v2.attackGroup then
+						local gr = Group.getByName(v2.attackGroup)
+						if gr then
+							gr:destroy()
+						end
 					end
 					
-					local gr = Group.getByName(v2.cap)
-					if gr then
-						gr:destroy()
+					if v2.cap then
+						local capgr = Group.getByName(v2.cap)
+						if capgr then
+							capgr:destroy()
+						end
 					end
 				end
 			end
@@ -111,20 +131,49 @@ do
 		mist.scheduleFunction(farpCommander.makeDecissions, {}, timer.getTime() + 1, 10)
 		
 		mist.scheduleFunction(farpCommander.makeDispatches, {}, timer.getTime() + farpCommander.aiSpawnerDelay, farpCommander.aiSpawnerDelay)
+		
+		--add event handler to handle birth events
+		local ev = {}
+		function ev:onEvent(event)
+			if event.id == 15 and event.initiator and event.initiator:getCategory() == Object.Category.UNIT then
+				local pname = event.initiator:getPlayerName()
+				if pname then
+					for index, farp in ipairs(farpCommander.farps) do
+						if event.initiator:getCoalition() ~= farp.side then
+							local zn = trigger.misc.getZone(farp.zone)
+							local dst = mist.utils.get3DDist(event.initiator:getPosition().p,zn.point)
+							if dst<zn.radius then
+								trigger.action.outTextForGroup(event.initiator:getGroup():getID(), 'Can not spawn in neutral/enemy zone '..farp.zone,5)
+								event.initiator:destroy()
+								break
+							end
+						end
+					end
+				end
+			end
+		end
+		
+		world.addEventHandler(ev)
 	end
 	
 	function farpCommander.makeDispatches()
 		for i,v in ipairs(farpCommander.farps) do
 			if v.redDispatcher and v.side == 1 then
-				if farpCommander.isFullyUpgraded(v) then
-					farpCommander.decideWhatToSend(v)
-				end
+				farpCommander.decideWhatToSend(v)
 			elseif v.blueDispatcher and v.side == 2 then
-				if farpCommander.isFullyUpgraded(v) then
-					farpCommander.decideWhatToSend(v)
-				end
+				farpCommander.decideWhatToSend(v)
 			end			
 		end
+	end
+	
+	function farpCommander.getFarpByName(name)
+		for i,v in ipairs(farpCommander.farps) do
+			if v.zone == name then 
+				return v
+			end
+		end
+		
+		return nil
 	end
 	
 	function farpCommander.decideWhatToSend(initiatorfarp)
@@ -139,56 +188,60 @@ do
 			local targetfarp = farpCommander.getFarpByZone(v.tozone)
 			if targetfarp then
 				-- check if supply needed
-				local un = Unit.getByName(v.supplyUnit)
-				if un and (un:getCategory()==Unit.Category.AIRPLANE or un:getCategory()==Unit.Category.HELICOPTER) and farpCommander.isLanded(un) then
-					un:destroy()
-				end
-				
-				un = Unit.getByName(v.supplyUnit)
-				if not un then
-					if targetfarp.side == initiatorfarp.side or targetfarp.side == 0 then
-						if not farpCommander.isFullyUpgraded(targetfarp) then
-							trigger.action.outText(initiatorfarp.zone..' sending supplies to '..targetfarp.zone, 5)
-							mist.respawnGroup(v.supplyUnit,true)
+				if v.supplyUnit then
+					local un = Unit.getByName(v.supplyUnit)
+					if un and (un:getCategory()==Unit.Category.AIRPLANE or un:getCategory()==Unit.Category.HELICOPTER) and farpCommander.isLanded(un) then
+						un:destroy()
+					end
+					
+					un = Unit.getByName(v.supplyUnit)
+					if not un then
+						if targetfarp.side == initiatorfarp.side or targetfarp.side == 0 then
+							if not farpCommander.isFullyUpgraded(targetfarp) then
+								trigger.action.outText(initiatorfarp.zone..' sending supplies to '..targetfarp.zone, 5)
+								mist.respawnGroup(v.supplyUnit,true)
+							end
 						end
 					end
 				end
 				
 				-- check if attack needed
-				
-				local gr = Group.getByName(v.attackGroup)
-				if gr then
-					for ix,vx in ipairs(gr:getUnits()) do
-						if (vx:getCategory()==Unit.Category.AIRPLANE or vx:getCategory()==Unit.Category.HELICOPTER) and farpCommander.isLanded(vx) then
-							vx:destroy()
+				if v.attackGroup then
+					local gr = Group.getByName(v.attackGroup)
+					if gr then
+						for ix,vx in ipairs(gr:getUnits()) do
+							if (vx:getCategory()==Unit.Category.AIRPLANE or vx:getCategory()==Unit.Category.HELICOPTER) and farpCommander.isLanded(vx) then
+								vx:destroy()
+							end
 						end
-				    end
-				end
-				
-				gr = Group.getByName(v.attackGroup)
-				if not gr then
-					if targetfarp.side ~= 0 and targetfarp.side ~= initiatorfarp.side then
-						trigger.action.outText(initiatorfarp.zone..' attacking '..targetfarp.zone, 5)
-						mist.respawnGroup(v.attackGroup,true)
+					end
+					
+					gr = Group.getByName(v.attackGroup)
+					if not gr then
+						if targetfarp.side ~= 0 and targetfarp.side ~= initiatorfarp.side then
+							trigger.action.outText(initiatorfarp.zone..' attacking '..targetfarp.zone, 5)
+							mist.respawnGroup(v.attackGroup,true)
+						end
 					end
 				end
 				
 				-- check if cap needed
-				
-				local gr = Group.getByName(v.cap)
-				if gr then
-					for ix,vx in ipairs(gr:getUnits()) do
-						if (vx:getCategory()==Unit.Category.AIRPLANE or vx:getCategory()==Unit.Category.HELICOPTER) and farpCommander.isLanded(vx) then
-							vx:destroy()
+				if v.cap then
+					local capgr = Group.getByName(v.cap)
+					if capgr then
+						for ix,vx in ipairs(capgr:getUnits()) do
+							if (vx:getCategory()==Unit.Category.AIRPLANE or vx:getCategory()==Unit.Category.HELICOPTER) and farpCommander.isLanded(vx) then
+								vx:destroy()
+							end
 						end
-				    end
-				end
-				
-				gr = Group.getByName(v.cap)
-				if not gr then
-					if targetfarp.side == initiatorfarp.side then
-						trigger.action.outText(initiatorfarp.zone..' sending patrol to '..targetfarp.zone, 5)
-						mist.respawnGroup(v.cap,true)
+					end
+					
+					capgr = Group.getByName(v.cap)
+					if not capgr then
+						if targetfarp.side == initiatorfarp.side then
+							trigger.action.outText(initiatorfarp.zone..' sending patrol to '..targetfarp.zone, 5)
+							mist.respawnGroup(v.cap,true)
+						end
 					end
 				end
 			end
@@ -196,7 +249,7 @@ do
 	end
 	
 	function farpCommander.isLanded(unit)
-		if (farpCommander.getAGL(unit)<5 and mist.vec.mag(unit:getVelocity())<0.10) or (not unit:inAir()) then
+		if (farpCommander.getAGL(unit)<5 and mist.vec.mag(unit:getVelocity())<0.10) then
 			return true
 		else
 			return false
@@ -263,7 +316,7 @@ do
 					trigger.action.setMarkupColor(index, farpCommander.redColor)
 					for i2,v2 in pairs(farp.upgradesRed) do
 						if not farp.builtupgrades[i2] then
-							local gr = mist.cloneInZone(v2, farp.zone)
+							local gr = mist.cloneInZone(v2, farp.zone, nil, nil, {initTasks=true, validTerrain={'LAND'}})
 							farp.builtupgrades[i2] = gr.name
 							break
 						end
@@ -274,7 +327,7 @@ do
 					trigger.action.setMarkupColor(index, farpCommander.blueColor)
 					for i2,v2 in pairs(farp.upgradesBlue) do
 						if not farp.builtupgrades[i2] then
-							local gr = mist.cloneInZone(v2, farp.zone)
+							local gr = mist.cloneInZone(v2, farp.zone, nil, nil, {initTasks=true, validTerrain={'LAND'}})
 							farp.builtupgrades[i2] = gr.name
 							break
 						end
@@ -293,7 +346,7 @@ do
 				for i2,v2 in pairs(upgrades) do
 					if not farp.builtupgrades[i2] then
 						crate:destroy()
-						local gr = mist.cloneInZone(v2, farp.zone)
+						local gr = mist.cloneInZone(v2, farp.zone, nil, nil, {initTasks=true, validTerrain={'LAND'}})
 						farp.builtupgrades[i2] = gr.name
 						trigger.action.outText(farp.zone..' ugpraded', 5)
 						break
@@ -320,7 +373,7 @@ do
 					trigger.action.setMarkupColor(index, farpCommander.redColor)
 					for i2,v2 in pairs(farp.upgradesRed) do
 						if not farp.builtupgrades[i2] then
-							local gr = mist.cloneInZone(v2, farp.zone)
+							local gr = mist.cloneInZone(v2, farp.zone, nil, nil, {initTasks=true, validTerrain={'LAND'}})
 							farp.builtupgrades[i2] = gr.name
 							break
 						end
@@ -331,7 +384,7 @@ do
 					trigger.action.setMarkupColor(index, farpCommander.blueColor)
 					for i2,v2 in pairs(farp.upgradesBlue) do
 						if not farp.builtupgrades[i2] then
-							local gr = mist.cloneInZone(v2, farp.zone)
+							local gr = mist.cloneInZone(v2, farp.zone, nil, nil, {initTasks=true, validTerrain={'LAND'}})
 							farp.builtupgrades[i2] = gr.name
 							break
 						end
@@ -350,7 +403,7 @@ do
 				for i2,v2 in pairs(upgrades) do
 					if not farp.builtupgrades[i2] then
 						cpUnit:destroy()
-						local gr = mist.cloneInZone(v2, farp.zone)
+						local gr = mist.cloneInZone(v2, farp.zone, nil, nil, {initTasks=true, validTerrain={'LAND'}})
 						farp.builtupgrades[i2] = gr.name
 						trigger.action.outText(farp.zone..' ugpraded', 5)
 						break
@@ -398,40 +451,44 @@ do
 		local pt = object:getPoint()
 		return pt.y - land.getHeight({ x = pt.x, y = pt.z })
 	end
+	
+	function farpCommander.getPointOnSurface(point)
+		return {x = point.x, y = land.getHeight({x = point.x, y = point.z}), z= point.z}
+	end
 end
 
-c1bluedp = {
-	{ tozone = "c2", supplyUnit = "bluecap1", attackGroup = "blueattack1", cap="bluecap1" }
-}
+-- c1bluedp = {
+	-- { tozone = "c2", supplyUnit = "bluecap1", attackGroup = "blueattack1", cap="blue1" }
+-- }
 
-c2bluedp = {
-	{ tozone = "c3", supplyUnit = "bluecap2", attackGroup = "blueattack2", cap="bluecap2" }
-}
+-- c2bluedp = {
+	-- { tozone = "c3", supplyUnit = "bluecap2", attackGroup = "blueattack2", cap="blue2" }
+-- }
 
-c3bluedp = {
-	{ tozone = "c4", supplyUnit = "bluecap3", attackGroup = "blueattack3", cap="bluecap3" }
-}
+-- c3bluedp = {
+	-- { tozone = "c4", supplyUnit = "bluecap3", attackGroup = "blueattack3", cap="blue3" }
+-- }
 
-c4reddp = {
-	{ tozone = "c3", supplyUnit = "redcap3", attackGroup = "redattack3", cap="redcap3" }
-}
+-- c4reddp = {
+	-- { tozone = "c3", supplyUnit = "redcap3", attackGroup = "redattack3", cap="red3" }
+-- }
 
-c3reddp = {
-	{ tozone = "c2", supplyUnit = "redcap2", attackGroup = "redattack2", cap="redcap2" }
-}
+-- c3reddp = {
+	-- { tozone = "c2", supplyUnit = "redcap2", attackGroup = "redattack2", cap="red2" }
+-- }
 
-c2reddp = {
-	{ tozone = "c1", supplyUnit = "redcap1", attackGroup = "redattack1", cap="redcap1"}
-}
+-- c2reddp = {
+	-- { tozone = "c1", supplyUnit = "redcap1", attackGroup = "redattack1", cap="red1"}
+-- }
 
-farpCommander.addFarp("c1", 2, 3, {"redcap2"}, { "blue1", "blue2", "blue3"}, { "red1", "red2", "red3"}, {}, nil, c1bluedp)
-farpCommander.addFarp("c2", 0, 0, {"bluecap1","redcap3"}, { "blue1", "blue2", "blue3"}, { "red1", "red2", "red3"}, {}, c2reddp, c2bluedp)
-farpCommander.addFarp("c3", 0, 0, {"redcap3", "bluecap2"}, { "blue1", "blue2", "blue3"}, { "red1", "red2", "red3"}, {}, c3reddp, c3bluedp)
-farpCommander.addFarp("c4", 1, 3, {"bluecap3"}, { "blue1", "blue2", "blue3"}, { "red1", "red2", "red3"}, {}, c4reddp, nil)
-farpCommander.aiSpawnerDelay = 300
-farpCommander.connections = {
-	{ from="c1", to="c2"},
-	{ from="c2", to="c3"},
-	{ from="c3", to="c4"}
-}
-farpCommander.init()
+-- farpCommander.addFarp("c1", 2, 3, {"redcap2"}, { "blue1", "blue2", "blue3"}, { "red1", "red2", "red3"}, {}, nil, c1bluedp)
+-- farpCommander.addFarp("c2", 0, 0, {"bluecap1","redcap3"}, { "blue1", "blue2", "blue3"}, { "red1", "red2", "red3"}, {}, c2reddp, c2bluedp)
+-- farpCommander.addFarp("c3", 0, 0, {"redcap3", "bluecap2"}, { "blue1", "blue2", "blue3"}, { "red1", "red2", "red3"}, {}, c3reddp, c3bluedp)
+-- farpCommander.addFarp("c4", 1, 3, {"bluecap3"}, { "blue1", "blue2", "blue3"}, { "red1", "red2", "red3"}, {}, c4reddp, nil)
+-- farpCommander.aiSpawnerDelay = 300
+-- farpCommander.connections = {
+	-- { from="c1", to="c2"},
+	-- { from="c2", to="c3"},
+	-- { from="c3", to="c4"}
+-- }
+-- farpCommander.init()
