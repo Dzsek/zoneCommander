6ad70bdb01e3b5c009c68541b5f0677c75c6822f
Dzsek <dzsekeb@gmail.com>
2021-11-07 15:07:28 +0200

    rewrote script to use classes, and much more intuitive setup, units now spawn on their own schedules

---
+++ ZoneCommanderDemoMission/zoneCommander.lua
@@ -0,0 +1,458 @@
+Utils = {}
+do
+	function Utils.getPointOnSurface(point)
+		return {x = point.x, y = land.getHeight({x = point.x, y = point.z}), z= point.z}
+	end
+	
+	function Utils.getAGL(object)
+		local pt = object:getPoint()
+		return pt.y - land.getHeight({ x = pt.x, y = pt.z })
+	end
+	
+	function Utils.isLanded(unit)
+		return (Utils.getAGL(unit)<5 and mist.vec.mag(unit:getVelocity())<0.10)
+	end
+	
+	function Utils.isInAir(unit)
+		return Utils.getAGL(unit)>5
+	end
+	
+	function Utils.isInZone(unit, zonename)
+		local zn = trigger.misc.getZone(zonename)
+		if zn then
+			local dist = mist.utils.get3DDist(unit:getPosition().p,zn.point)
+			return dist<zn.radius
+		end
+		
+		return false
+	end
+	
+	function Utils.isCrateSettledInZone(crate, zonename)
+		local zn = trigger.misc.getZone(zonename)
+		if zn and crate then
+			local dist = mist.utils.get3DDist(crate:getPosition().p,zn.point)
+			return (dist<zn.radius and Utils.getAGL(crate)<1)
+		end
+		
+		return false
+	end
+	
+	function Utils.someOfGroupInZone(group, zonename)
+		for i,v in pairs(group:getUnits()) do
+			if Utils.isInZone(v, zonename) then
+				return true
+			end
+		end
+		
+		return false
+	end
+	
+	function Utils.allGroupIsLanded(group)
+		for i,v in pairs(group:getUnits()) do
+			if not Utils.isLanded(v) then
+				return false
+			end
+		end
+		
+		return true
+	end
+	
+	function Utils.someOfGroupInAir(group)
+		for i,v in pairs(group:getUnits()) do
+			if Utils.isInAir(v) then
+				return true
+			end
+		end
+		
+		return false
+	end
+end
+
+GlobalSettings = {}
+do
+	GlobalSettings.blockedDespawnTime = 10*60
+	GlobalSettings.hangarRespawnTime = 2*60
+	GlobalSettings.deadRespawnTime = 15*60
+	GlobalSettings.landedDespawnTime = 1*60
+end
+
+
+BattleCommander = {}
+do
+	BattleCommander.zones = {}
+	BattleCommander.connections = {}
+	
+	function BattleCommander:new()
+		local obj = {}
+		setmetatable(obj, self)
+		self.__index = self
+		return obj
+	end
+	
+	--zone: ZoneCommander
+	function BattleCommander:addZone(zone)
+		table.insert(self.zones, zone)
+		zone.index = self:getZoneIndexByName(zone.zone)
+		zone.battleCommander = self
+	end
+	
+	function BattleCommander:getZoneByName(name)
+		for i,v in ipairs(self.zones) do
+			if v.zone == name then
+				return v
+			end
+		end
+	end
+	
+	function BattleCommander:addConnection(f, t)
+		table.insert(self.connections, {from=f, to=t})
+	end
+	
+	function BattleCommander:getZoneIndexByName(name)
+		for i,v in ipairs(self.zones) do
+			if v.zone == name then
+				return i
+			end
+		end
+	end
+	
+	function BattleCommander:getZones()
+		return self.zones
+	end
+	
+	function BattleCommander:init()
+		for i,v in ipairs(self.zones) do
+			v:init()
+		end
+		
+		for i,v in ipairs(self.connections) do
+			local from = trigger.misc.getZone(v.from)
+			local to = trigger.misc.getZone(v.to)
+			
+			trigger.action.lineToAll(-1, 1000+i, from.point, to.point, {1,1,1,0.5}, 2)
+		end
+		
+		mist.scheduleFunction(self.update, {self}, timer.getTime() + 1, 10)
+		
+		local ev = {}
+		function ev:onEvent(event)
+			if event.id == 15 and event.initiator and event.initiator:getCategory() == Object.Category.UNIT then
+				local pname = event.initiator:getPlayerName()
+				if pname then
+					for index, zc in ipairs(BattleCommander:getZones()) do
+						if event.initiator:getCoalition() ~= zc.side then
+							local zn = trigger.misc.getZone(zc.zone)
+							local dst = mist.utils.get3DDist(event.initiator:getPosition().p,zn.point)
+							if dst<zn.radius then
+								local tside = 'enemy'
+								if zc.side==0 then
+									tside = 'neutral'
+								end
+								
+								trigger.action.outTextForGroup(event.initiator:getGroup():getID(), 'Can not spawn in '..tside..' zone '..zc.zone,5)
+								event.initiator:destroy()
+								break
+							end
+						end
+					end
+				end
+			end
+		end
+		
+		world.addEventHandler(ev)
+	end
+	
+	function BattleCommander:update()
+		for i,v in ipairs(self.zones) do
+			v:update()
+		end
+	end
+end
+
+ZoneCommander = {}
+do
+	--{ zone='zonename', side=[0=neutral, 1=red, 2=blue], level=int, upgrades={red={}, blue={}}, crates={} }
+	function ZoneCommander:new(obj)
+		obj = obj or {}
+		obj.built = {}
+		obj.index = -1
+		obj.battleCommander = {}
+		obj.groups = {}
+		setmetatable(obj, self)
+		self.__index = self
+		return obj
+	end
+
+	function ZoneCommander:init()
+		local zone = trigger.misc.getZone(self.zone)
+		local color = {0.7,0.7,0.7,0.3}
+		if self.side == 1 then
+			color = {1,0,0,0.3}
+		elseif self.side == 2 then
+			color = {0,0,1,0.3}
+		end
+		
+		trigger.action.circleToAll(-1,self.index,zone.point, zone.radius,color,color,1)
+		trigger.action.textToAll(-1,2000+self.index,zone.point, {0,0,0,0.5}, {0,0,0,0}, 15, true, self.zone)
+		
+		if #self.built < self.level then
+			local upgrades
+			if self.side == 1 then
+				upgrades = self.upgrades.red
+			elseif self.side == 2 then
+				upgrades = self.upgrades.blue
+			else
+				upgrades = {}
+			end
+			
+			for i,v in pairs(upgrades) do
+				if not self.built[i] and i<=self.level then
+					local gr = mist.cloneInZone(v, self.zone, nil, nil, {initTasks=true, validTerrain={'LAND'}})
+					self.built[i] = gr.name
+				end
+			end
+		end
+		
+		for i,v in ipairs(self.groups) do
+			v:init()
+		end
+	end
+	
+	function ZoneCommander:update()
+		for i,v in pairs(self.built) do
+			local gr = Group.getByName(v)
+			if gr and gr:getSize() == 0 then
+				gr:destroy()
+			end
+			
+			if not gr or gr:getSize() == 0 then
+				self.built[i] = nil
+				trigger.action.outText(self.zone..' lost group '..v, 5)
+			end		
+		end
+		
+		local empty = true
+		for i,v in pairs(self.built) do
+			if v then
+				empty = false
+				break
+			end
+		end
+		
+		if empty and self.side ~= 0 then
+			self.side = 0
+			trigger.action.outText(self.zone..' is now neutral ', 5)
+			trigger.action.setMarkupColorFill(self.index, {0.7,0.7,0.7,0.3})
+			trigger.action.setMarkupColor(self.index, {0.7,0.7,0.7,0.3})
+		end
+		
+		for i,v in ipairs(self.groups) do
+			v:update()
+		end
+		
+		if self.crates then
+			for i,v in ipairs(self.crates) do
+				local crate = StaticObject.getByName(v)
+				if crate and Utils.isCrateSettledInZone(crate, self.zone) then
+					if self.side == 0 then
+						self:capture(crate:getCoalition())
+					elseif self.side == crate:getCoalition() then
+						self:upgrade()
+					end
+					
+					crate:destroy()
+				end
+			end
+		end
+	end
+	
+	function ZoneCommander:addGroup(group)
+		table.insert(self.groups, group)
+		group.zoneCommander = self
+	end
+	
+	function ZoneCommander:addGroups(groups)
+		for i,v in ipairs(groups) do
+			table.insert(self.groups, v)
+			v.zoneCommander = self
+		end
+	end
+	
+	function ZoneCommander:capture(newside)
+		if self.side == 0 and newside ~= 0 then
+			self.side = newside
+			
+			local sidename = ''
+			local color = {0.7,0.7,0.7,0.3}
+			if self.side==1 then
+				sidename='RED'
+				color = {1,0,0,0.3}
+			elseif self.side==2 then
+				sidename='BLUE'
+				color = {0,0,1,0.3}
+			end
+			
+			trigger.action.outText(self.zone..' captured by '..sidename, 5)
+			trigger.action.setMarkupColorFill(self.index, color)
+			trigger.action.setMarkupColor(self.index, color)
+			
+			self:upgrade()
+		end
+	end
+	
+	function ZoneCommander:upgrade()
+		if self.side ~= 0 then
+			local upgrades
+			if self.side == 1 then
+				upgrades = self.upgrades.red
+			elseif self.side == 2 then
+				upgrades = self.upgrades.blue
+			else
+				upgrades = {}
+			end
+				
+			if #self.built < #upgrades then
+				for i,v in pairs(upgrades) do
+					if not self.built[i] then
+						local gr = mist.cloneInZone(v, self.zone, nil, nil, {initTasks=true, validTerrain={'LAND'}})
+						self.built[i] = gr.name
+						trigger.action.outText(self.zone..' defenses upgraded', 5)
+						break
+					end
+				end
+			else
+				for i,v in pairs(self.built) do
+					local gr = Group.getByName(v)
+					if gr and gr:getSize() < gr:getInitialSize() then
+						mist.respawnGroup(v, true)
+						trigger.action.outText(self.zone..' resupplied group '..v, 5)
+						break
+					end
+				end
+			end
+		end
+	end
+end
+
+GroupCommander = {}
+do
+	--{ name='groupname', mission=['patrol', 'supply', 'attack'], targetzone='zonename' }
+	function GroupCommander:new(obj)
+		obj = obj or {}
+		obj.state = 'inhangar'
+		obj.lastStateTime = timer.getAbsTime()
+		obj.zoneCommander = {}
+		obj.side = 0
+		setmetatable(obj, self)
+		self.__index = self
+		return obj
+	end
+	
+	function GroupCommander:init()
+		self.state = 'inhangar'
+		self.lastStateTime = timer.getAbsTime()
+		local gr = Group.getByName(self.name)
+		if gr then
+			self.side = gr:getCoalition()
+			gr:destroy()
+		end
+	end
+	
+	function GroupCommander:shouldSpawn()
+		if self.side ~= self.zoneCommander.side then
+			return false
+		end
+		
+		local tg = self.zoneCommander.battleCommander:getZoneByName(self.targetzone)
+		if tg then
+			if self.mission=='patrol' then
+				if tg.side == self.side then
+					return true
+				end
+			elseif self.mission=='attack' then
+				if tg.side ~= self.side and tg.side ~= 0 then
+					return true
+				end
+			elseif self.mission=='supply' then
+				if tg.side == self.side or tg.side == 0 then
+					return true
+				end
+			end
+		end
+		
+		return false
+	end
+	
+	function GroupCommander:update()
+		local gr = Group.getByName(self.name)
+		if not gr or gr:getSize()==0 then
+			if gr and gr:getSize()==0 then
+				gr:destroy()
+			end
+		
+			if self.state ~= 'inhangar' and self.state ~= 'dead' then
+				self.state = 'dead'
+				self.lastStateTime = timer.getAbsTime()
+			end
+		end
+	
+		if self.state == 'inhangar' then
+			if timer.getAbsTime() - self.lastStateTime > GlobalSettings.hangarRespawnTime then
+				if self:shouldSpawn() then
+					mist.respawnGroup(self.name,true)
+					self.state = 'takeoff'
+					self.lastStateTime = timer.getAbsTime()
+				end
+			end
+		elseif self.state =='takeoff' then
+			if timer.getAbsTime() - self.lastStateTime > GlobalSettings.blockedDespawnTime then
+				if gr and Utils.allGroupIsLanded(gr) then
+					gr:destroy()
+					self.state = 'inhangar'
+					self.lastStateTime = timer.getAbsTime()
+				end
+			elseif gr and Utils.someOfGroupInAir(gr) then
+				self.state = 'inair'
+				self.lastStateTime = timer.getAbsTime()
+			end
+		elseif self.state =='inair' then
+			if gr and Utils.allGroupIsLanded(gr) then
+				self.state = 'landed'
+				self.lastStateTime = timer.getAbsTime()
+			end
+		elseif self.state =='landed' then
+			if self.mission == 'supply' then
+				local tg = self.zoneCommander.battleCommander:getZoneByName(self.targetzone)
+				if tg and gr and Utils.someOfGroupInZone(gr, tg.zone) then
+					gr:destroy()
+					self.state = 'dead'
+					self.lastStateTime = timer.getAbsTime()
+					if tg.side == 0 then
+						tg:capture(self.side)
+					elseif tg.side == self.side then
+						tg:upgrade()
+					end
+				end
+			end
+			
+			if timer.getAbsTime() - self.lastStateTime > GlobalSettings.landedDespawnTime then
+				if gr then 
+					gr:destroy()
+					self.state = 'inhangar'
+					self.lastStateTime = timer.getAbsTime()
+				end
+			end
+		elseif self.state =='dead' then
+			if timer.getAbsTime() - self.lastStateTime > GlobalSettings.deadRespawnTime then
+				if self:shouldSpawn() then
+					mist.respawnGroup(self.name,true)
+					self.state = 'takeoff'
+					self.lastStateTime = timer.getAbsTime()
+				end
+			end
+		end
+	end
+end
+
+
+
---
+++ ZoneCommanderDemoMission/zoneSetup.lua
@@ -0,0 +1,108 @@
+function merge(tbls)
+	local res = {}
+	for i,v in ipairs(tbls) do
+		for i2,v2 in ipairs(v) do
+			table.insert(res,v2)
+		end
+	end
+	
+	return res
+end
+
+airfield = {
+	blue = { "bInfantry", "bArmor", "bSam", "bSam2", "bSam3"},
+	red = {"rInfantry", "rArmor", "rSam", "rSam2", "rSam3" }
+}
+
+farp = {
+	blue = {"bInfantry", "bArmor", "bSam"},
+	red = {"rInfantry", "rArmor", "rSam" }
+}
+
+cargoSpawns = {
+	["Anapa"] = {"c1","c2","c3"},
+	["Bravo"] = {"c6","c7"},
+	["Krymsk"] = {"c8","c9","c10"}
+}
+
+cargoAccepts = {
+	anapa = merge({cargoSpawns.Bravo}),
+	alpha =  merge({cargoSpawns.Bravo,cargoSpawns.Anapa}),
+	bravo =  merge({cargoSpawns.Anapa}),
+	charlie =  merge({cargoSpawns.Anapa, cargoSpawns.Bravo}),
+	krymsk =  merge({cargoSpawns.Bravo, cargoSpawns.Anapa})
+}
+
+bc = BattleCommander:new()
+anapa = ZoneCommander:new({zone='Anapa', side=2, level=5, upgrades=airfield, crates=cargoAccepts.anapa})
+alpha = ZoneCommander:new({zone='Alpha', side=0, level=0, upgrades=farp, crates=cargoAccepts.alpha})
+bravo = ZoneCommander:new({zone='Bravo', side=1, level=3, upgrades=farp, crates=cargoAccepts.bravo})
+charlie = ZoneCommander:new({zone='Charlie', side=0, level=0, upgrades=farp, crates=cargoAccepts.charlie})
+krymsk = ZoneCommander:new({zone='Krymsk', side=1, level=5, upgrades=farp, crates=cargoAccepts.krymsk})
+
+
+dispatch = {
+	krymsk = {
+		GroupCommander:new({name='krym1', mission='supply', targetzone='Bravo'}),
+		GroupCommander:new({name='krym2', mission='attack', targetzone='Bravo'}),
+		GroupCommander:new({name='krym3', mission='patrol', targetzone='Bravo'}),
+		GroupCommander:new({name='krym4', mission='patrol', targetzone='Krymsk'})
+	},
+	bravo = {
+		GroupCommander:new({name='bravo1', mission='supply', targetzone='Alpha'}),
+		GroupCommander:new({name='bravo2', mission='attack', targetzone='Alpha'}),
+		GroupCommander:new({name='bravo6', mission='supply', targetzone='Charlie'}),
+		GroupCommander:new({name='bravo7', mission='attack', targetzone='Charlie'}),
+		GroupCommander:new({name='bravo4', mission='supply', targetzone='Krymsk'}),
+		GroupCommander:new({name='bravo5', mission='attack', targetzone='Krymsk'})
+	},
+	anapa = {
+		GroupCommander:new({name='anapa1', mission='supply', targetzone='Alpha'}),
+		GroupCommander:new({name='anapa3', mission='supply', targetzone='Bravo'}),
+		GroupCommander:new({name='anapa2', mission='supply', targetzone='Charlie'}),
+		GroupCommander:new({name='anapa4', mission='supply', targetzone='Krymsk'})
+	}
+}
+
+
+anapa:addGroups(dispatch.anapa)
+bravo:addGroups(dispatch.bravo)
+krymsk:addGroups(dispatch.krymsk)
+
+bc:addZone(anapa)
+bc:addZone(alpha)
+bc:addZone(bravo)
+bc:addZone(charlie)
+bc:addZone(krymsk)
+
+bc:addConnection("Anapa","Alpha")
+bc:addConnection("Alpha","Bravo")
+bc:addConnection("Bravo","Krymsk")
+bc:addConnection("Bravo","Charlie")
+bc:addConnection("Anapa","Charlie")
+
+bc:init()
+
+function respawnCargo()
+	for i,v in pairs(cargoSpawns) do
+		local farp = bc:getZoneByName(i)
+		if farp then
+			if farp.side==2 then
+				for ix,vx in ipairs(v) do
+					if not StaticObject.getByName(vx) then
+						mist.respawnGroup(vx)
+					end
+				end
+			else 
+				for ix,vx in ipairs(v) do
+					local st = StaticObject.getByName(vx)
+					if st then
+						st:destroy()
+					end
+				end
+			end
+		end
+	end
+end
+
+mist.scheduleFunction(respawnCargo, {}, timer.getTime() + 1, 30)
