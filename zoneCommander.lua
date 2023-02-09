55a3442e0fd7e2e0fbbd5d37f916b5223411f5bd
Gold John King <GoldJohnKing@Live.cn>
2023-02-09 15:37:31 +0800

    Fix unit not exists error when Hercules dies before cargo reaches ground

--- zoneCommander.lua
+++ zoneCommander.lua
@@ -3284,7 +3284,7 @@ do
 	function HercCargoDropSupply.ProcessCargo(shotevent)
 		local cargo = shotevent.weapon
 		local zn = HercCargoDropSupply.battleCommander:getZoneOfWeapon(cargo)
-		if zn and zn.active and shotevent.initiator then
+		if zn and zn.active and shotevent.initiator and shotevent.initiator:isExist() then
 			local herc = HercCargoDropSupply.herculesRegistry[shotevent.initiator:getName()]
 			if not herc or herc.takeoffzone == zn.zone then
 				cargo:destroy()
